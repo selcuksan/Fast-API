6ee650bef3086d5d927af8dddd1403e0c42a6637
Selçuk Şan <56341239+selcuksan@users.noreply.github.com>
2023-09-05 17:12:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 from blog import models
 from blog.database import engine
 from blog.routers import blog, user, authentication
@@ -11,3 +11,6 @@ models.Base.metadata.create_all(engine)
 app.include_router(blog.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
